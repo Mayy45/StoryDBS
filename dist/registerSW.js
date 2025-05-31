040f7e6f827656e31b5b7fb71bda2704c78f6f40
Mayy45 <pakrt556@gmail.com>
2025-05-31 22:01:04 +0700

    Configure Vite for GitHub Pages deployment

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/StoryDBS/sw.js', { scope: '/StoryDBS/' })})}
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base:'/StoryDBS/',
   server: {
     proxy: {
       '/v1': {
