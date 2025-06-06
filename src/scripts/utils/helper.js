e756e5dfcf71c6db21b38c72947c55ea08abe7a3
Mayy45 <pakrt556@gmail.com>
2025-06-06 10:29:45 +0700

    Baruu

--- src/scripts/utils/helper.js
+++ src/scripts/utils/helper.js
@@ -1,11 +1,7 @@
 export function urlBase64ToUint8Array(base64String) {
-  // Tambahkan padding '=' supaya panjang string kelipatan 4
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
-  // Ganti karakter URL-safe base64 menjadi base64 biasa
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
-  // Decode base64 menjadi string biner
   const rawData = atob(base64);
-  // Buat Uint8Array dari kode ASCII setiap karakter
   const outputArray = new Uint8Array(rawData.length);
   for (let i = 0; i < rawData.length; ++i) {
     outputArray[i] = rawData.charCodeAt(i);
